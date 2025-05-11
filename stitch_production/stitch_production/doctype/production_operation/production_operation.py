abc60c085452bca471726eae78d1ff2d5162fb45
SamiDev016 <adelim0555@gmail.com>
2025-05-11 11:11:10 +0000

    final production version

--- stitch_production/stitch_production/doctype/production_operation/production_operation.py
+++ stitch_production/stitch_production/doctype/production_operation/production_operation.py
@@ -1,8 +1,9 @@
+# Copyright (c) 2025, samidev016 and contributors
+# For license information, please see license.txt
+
 import frappe
 from frappe.model.document import Document
 from frappe import _
-import random
-import string
 
 class ProductionOperation(Document):
 
@@ -12,81 +13,42 @@ class ProductionOperation(Document):
         if not self.produced_quantity:
             frappe.throw(_("Please enter a quantity to produce"))
 
+        # Get item from BOM
         bom = frappe.get_doc("BOM", self.poduction_bom)
         self.production_item = bom.item
 
-        if not self.raw_materials:
-            for bi in bom.items:
-                required = (bi.qty or 0) * self.produced_quantity / (bom.quantity or 1)
-                self.append("raw_materials", {
-                    "material":          bi.item_code,
-                    "batch_no":          None,
-                    "required_quantity": required,
-                    "item_warehouse":    bom.fg_warehouse
-                })
-
     def on_submit(self):
-        # 1) Create a named batch for the FG
-        fg_batch = self._make_named_batch(self.production_item)
-        # store it for reference
-        self.db_set("finish_good_batch", fg_batch)
-
-        # 2) Build maps for raw materials
-        batch_map = {r.material: r.batch_no for r in self.raw_materials}
-        qty_map   = {r.material: r.required_quantity for r in self.raw_materials}
-        wh_map    = {r.material: r.item_warehouse for r in self.raw_materials}
-
-        # 3) Start the Stock Entry
         se = frappe.new_doc("Stock Entry")
-        se.update({
-            "stock_entry_type":   "Manufacture",
-            "bom_no":             self.poduction_bom,
-            "production_item":    self.production_item,
-            "produced_qty":       self.produced_quantity,
-            "fg_completed_qty":   self.produced_quantity,
-            "fg_warehouse":       self.warehouse,
-            "from_bom":           1
-        })
+        se.stock_entry_type     = "Manufacture"
+        se.bom_no               = self.poduction_bom
+        se.production_item      = self.production_item
+        se.fg_warehouse         = self.warehouse
+        se.produced_qty         = self.produced_quantity
+        se.from_bom             = 0  # we are manually adding all items
+        se.company              = self.company
+        se.set_posting_time     = 1
 
-        # 4) Add Finished Good FIRST
+        # Add Finished Good row
         se.append("items", {
-            "item_code":         self.production_item,
-            "qty":               self.produced_quantity,
-            "transfer_qty":      self.produced_quantity,
-            "t_warehouse":       self.warehouse,
-            "batch_no":          fg_batch,
-            "create_new_batch":  1,
-            "uom":               frappe.db.get_value("Item", self.production_item, "stock_uom"),
-            "conversion_factor": 1
+            "item_code": self.production_item,
+            "qty": self.produced_quantity,
+            "t_warehouse": self.warehouse,
+            "is_finished_item": 1,
+            "allow_zero_valuation_rate": 1
         })
 
-        # 5) Then add each raw-material line
-        for mat in batch_map:
+        # Add Raw Material rows from your child table
+        for row in self.raw_materials:
             se.append("items", {
-                "item_code":         mat,
-                "qty":               qty_map[mat],
-                "transfer_qty":      qty_map[mat],
-                "s_warehouse":       wh_map[mat],
-                "batch_no":          batch_map[mat],
-                "create_new_batch":  0,
-                "uom":               frappe.db.get_value("Item", mat, "stock_uom"),
-                "conversion_factor": 1
+                "item_code": row.material,
+                "qty": row.required_quantity,
+                "s_warehouse": row.item_warehouse,
+                "batch_no": row.batch_no,
+                "allow_zero_valuation_rate": 1
             })
 
-        # 6) Save & submit
+        # Save and submit
         se.insert(ignore_permissions=True)
         se.submit()
 
-        # 7) Link back
         self.db_set("stock_entry", se.name)
-
-    def _make_named_batch(self, item_code):
-        suffix = ''.join(random.choices(string.digits, k=4))
-        batch_id = f"Production-{self.name}-{suffix}"
-        batch = frappe.get_doc({
-            "doctype": "Batch",
-            "item":     item_code,
-            "batch_id": batch_id
-        })
-        batch.insert(ignore_permissions=True)
-        return batch.name
