fd27768a39232fa1ba9bc6dd8e71f08afe76f648
SamiDev016 <samiadel@SamiPc>
2025-06-27 20:26:58 +0100

    fetch boms

--- stitch_production/stitch_production/doctype/assemblying/assemblying.js
+++ stitch_production/stitch_production/doctype/assemblying/assemblying.js
@@ -1,8 +1,41 @@
-// Copyright (c) 2025, samidev016 and contributors
-// For license information, please see license.txt
+function setup_main_bom_query(frm) {
+    frm.set_df_property("main_bom", "hidden", 1);
 
-// frappe.ui.form.on("Assemblying", {
-// 	refresh(frm) {
+    if (frm.doc.main_operation) {
+        frappe.call({
+            method: "frappe.client.get",
+            args: {
+                doctype: "cutting operation",
+                name: frm.doc.main_operation
+            },
+            callback: function(r) {
+                if (!r.message) return;
 
-// 	},
-// });
+                let bom_list = (r.message.parent_boms || []).map(row => row.parent_bom);
+
+                if (bom_list.length) {
+                    frm.set_df_property("main_bom", "hidden", 0);
+
+                    frm.set_query("main_bom", function() {
+                        return {
+                            filters: [
+                                ["name", "in", bom_list]
+                            ]
+                        };
+                    });
+                } else {
+                    frappe.msgprint("No BOMs linked to the selected Main Operation.");
+                }
+            }
+        });
+    }
+}
+
+frappe.ui.form.on("Assemblying", {
+    onload_post_render(frm) {
+        setup_main_bom_query(frm);
+    },
+    main_operation: function(frm) {
+        setup_main_bom_query(frm);
+    }
+});
